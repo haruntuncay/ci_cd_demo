5e55faa9d47c05f00069b1c818de197d25063f7b
Harun Tuncay <haruntuncay@live.com>
2022-04-14 13:26:16 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,13 +1,14 @@
 from obj import MyClass
 
 def increment(n=1):  
-    return n + 1 
+    print(n+1)
+    print(
 
 def multiply(n=1, m=10):
     return n * n
 
 def mysum(arr):
-    print(su m(arr))
+    print(sum(arr))
 
 def object_test(obj1, obj2):
     obj3 = MyClass(obj1.x, obj2.y)
